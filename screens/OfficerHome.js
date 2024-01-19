27d4f0135b1003b0eb27810604578c7c5af25967
Desh1100 <deshansameera1100@gmail.com>
2024-01-19 13:28:34 +0530

    Update OfficerHome.js header
    have to make 4 Boxes

--- screens/OfficerHome.js
+++ screens/OfficerHome.js
@@ -3,86 +3,51 @@ import { StyleSheet, View, Text } from "react-native";
 import { Image } from "expo-image";
 import { Color, FontFamily, Border, FontSize } from "../GlobalStyles";
 
-const IPhone1415ProMax2 = () => {
+const OfficerHome = () => {
   return (
     <View style={styles.bg}>
-      <View style={styles.headerA} />
-      <Image
-        style={styles.lrdcLogo3}
-        contentFit="cover"
-        source={require("../assets/lrdc-logo-2.png")}
-      />
-      <Text style={[styles.lrdcBoatApp, styles.lrdcBoatAppTypo]}>
-        LRDC BOAT APP
-      </Text>
-      <Text>Home</Text>
+      <View style={styles.headerA}>
+        <Image
+          style={styles.lrdcLogo3}
+          contentFit="cover"
+          source={require("../assets/lrdc-logo-2.png")}
+        />
+        <View style={{ left: "25%", top: -20 }}>
+          <Text style={{ fontSize: 18, fontWeight: "bold" }}>
+            LRDC BOAT APP
+          </Text>
+          <Text>Home</Text>
+        </View>
+      </View>
+
+      <View style={{ height: "20%", width: "20%", backgroundColor: "#000" }} />
+      <View style={{ height: "20%", width: "20%", backgroundColor: "#000" }} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  lrdcBoatAppTypo: {
-    textAlign: "left",
-    color: Color.colorBlack,
-    fontFamily: FontFamily.inter,
-    position: "absolute",
+  headerA: {
+    borderBottomRightRadius: Border.br_38xl,
+    width: "100%",
+    height: "18%",
+    backgroundColor: Color.colorPalegoldenrod,
   },
 
-  headerA: {
-    marginLeft: -239,
-    top: -16,
-    borderBottomRightRadius: Border.br_38xl,
-    width: 454,
-    height: 184,
-    backgroundColor: Color.colorPalegoldenrod,
-    left: "50%",
-    position: "absolute",
-  },
   lrdcLogo3: {
-    marginLeft: -200,
-    top: 66,
-    width: 105,
-    height: 92,
-    left: "50%",
-    position: "absolute",
+    top: "30%",
+    width: "20%",
+    height: "65%",
   },
-  lrdcBoatApp: {
-    top: 76,
-    left: 136,
-    fontSize: FontSize.size_5xl,
-  },
-  reservations: {
-    top: 105,
-    left: 230,
-    fontSize: FontSize.size_base,
-  },
-  iphone1415ProMax4Item: {
-    top: 826,
-    borderRadius: Border.br_31xl,
-    height: 67,
-    width: 350,
-    marginLeft: -175,
-    backgroundColor: Color.colorPalegoldenrod,
-    left: "50%",
-    position: "absolute",
-  },
-  iphone1415ProMax4Inner: {
-    top: 224,
-  },
-  rectangleView: {
-    top: 401,
-  },
-  iphone1415ProMax4Child1: {
-    top: 578,
-  },
+
   bg: {
     borderRadius: Border.br_6xl,
     backgroundColor: Color.colorFloralwhite_100,
     flex: 1,
     width: "100%",
-    height: 932,
     overflow: "hidden",
+    alignItems: "center",
   },
 });
 
-export default IPhone1415ProMax2;
+export default OfficerHome;
