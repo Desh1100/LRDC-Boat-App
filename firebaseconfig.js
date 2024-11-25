8701d5e76bde1c2ccde033851e69c24be51626dc
SMDS Senarathna <110003524+Desh1100@users.noreply.github.com>
2024-11-25 11:20:44 +0530

    Update firebaseconfig.js
    firebase api key updated

--- firebaseconfig.js
+++ firebaseconfig.js
@@ -6,12 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 //  Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAtHgnVsamgf76y3cE-1OBFNmGECbcbhsE",
-  authDomain: "lrdc-boat-app.firebaseapp.com",
+  apiKey: "",//add your firebase apikey
+  authDomain: "",
   projectId: "lrdc-boat-app",
   storageBucket: "lrdc-boat-app.appspot.com",
   messagingSenderId: "820531552490",
-  appId: "1:820531552490:web:a144a3e7e3ccac6cbab793",
+  appId: "",
   measurementId: "G-7KGWS52JJV",
 };
 
